26cef02cba9b425b3937e3770f537020850499ae
Renato Mello <renatogsmello@gmail.com>
2023-08-01 20:15:22 -0300

    Criaçao administraçao e inclusao de restaurante

---
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -0,0 +1,24 @@
+import { Button, TextField } from "@mui/material"
+import axios from "axios"
+import { useState } from "react"
+
+export default function FormularioRestaurante() {
+	const [nomeRestaurante, setNomeRestaurante] = useState("")
+
+	const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+		evento.preventDefault()
+		console.log("quero enviar dados para api:")
+		console.log(nomeRestaurante)
+
+		axios.post("http://localhost:8000/api/v2/restaurantes/", { nome: nomeRestaurante }).then(() => alert("Restaurante cadastrado com sucesso!"))
+	}
+
+	return (
+		<form onSubmit={aoSubmeterForm}>
+			<TextField onChange={(evento) => setNomeRestaurante(evento.target.value)} label="Nome do Restaurante" variant="standard" />
+			<Button type="submit" variant="outlined">
+				Salvar
+			</Button>
+		</form>
+	)
+}
